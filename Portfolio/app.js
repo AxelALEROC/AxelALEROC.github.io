68714805b7a1914d4858254009378f56a3ae5eac
AxelALEROC <alejosaxelr@gmail.com>
2023-11-06 10:36:17 -0600

    Inserción del apartado de proyectos.

--- Portfolio/app.js
+++ Portfolio/app.js
@@ -17,3 +17,21 @@ links.forEach(function(link){
     })
 
 });
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("proyecto__img");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
